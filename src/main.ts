abd3ba394ad0b8af381db9db2836085d30bec74a
小黎（家） <22708206@qq.com>
2023-01-14 23:28:14 +0800

    feat(main.ts): 新增控制台输出介绍
    新增控制台输出介绍

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import ElementPlus from 'element-plus'
 import './styles/index.less'
 import 'element-plus/dist/index.css'
 
-
 const app = createApp(App)
-
 app.use(ElementPlus).mount('#app')
+
+console.log('%c 🐇年春节头像定制🌈 | 黎 | https://github.com/xiaoli1999/custom-rabbitImage ', 'color: #f4f4f4;background: #444; padding:5px 0;border-radius:2px;')
+
