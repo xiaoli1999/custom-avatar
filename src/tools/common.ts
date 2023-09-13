fdb7c1f65d763fbd2dc5f4b7720f8b1f6492403c
小黎 <22708206@qq.com>
2023-09-13 17:33:03 +0800

    feat(index.html、tools/commons.ts): 引入hash算法，新增计算又拍云签名算法
    引入hash算法，新增计算又拍云签名算法

--- src/tools/common.ts
+++ src/tools/common.ts
@@ -78,3 +78,125 @@ export const downloadImg = (url: string, name: string) => {
     a.dispatchEvent(event)
     a.remove()
 }
+
+/**
+ * @function getAuthorization 获取授权
+ * @param { Object } userInfo 用户信息
+ * @param { String } userInfo.bucket 文服务名称
+ * @param { String } userInfo.name 用户名称
+ * @param { String } userInfo.password 用户密码
+ * @param { String } userInfo.path 查找目录途径
+ * @return { String } date 大写时间
+ * @return { String } authorization 签名
+ */
+export const getAuthorization = (userInfo: any = {}) => {
+    const info = JSON.parse(JSON.stringify(userInfo))
+    info.bucket = atob(info.bucket)
+    info.name = atob(info.name)
+    info.password = atob(info.password)
+
+    const { HexMD5, b64hamcsha1 }  = (window || {}) as any
+
+    const date = (new Date).toUTCString()
+
+    /* 计算 Authorization */
+    const passwordMd5 = HexMD5.MD5(info.password).toString(HexMD5.enc.Hex)
+
+    /* [Method-请求方法, URI-请求路径, date] */
+    const arr = ['GET', `/${ info.bucket }/${ info.path }`, date]
+
+    const authorization = `UPYUN ${ info.name }:${ b64hamcsha1(passwordMd5, arr.join('&')) }`
+
+    return { date, authorization }
+}
+
+/**
+ * @function getUploadAuthorization 获取授权
+ * @param { Object } userInfo 用户信息
+ * @param { String } userInfo.bucket 文服务名称
+ * @param { String } userInfo.name 用户名称
+ * @param { String } userInfo.password 用户密码
+ * @param { String } userInfo.path 上传途径
+ * @return { String } policy 策略
+ * @return { String } authorization 签名
+ */
+export const getUploadAuthorization = (userInfo: any = {}) => {
+    const info = JSON.parse(JSON.stringify(userInfo))
+    info.bucket = atob(info.bucket)
+    info.name = atob(info.name)
+    info.password = atob(info.password)
+
+    const policyObj = {
+        bucket: info.bucket,
+        'save-key': `${ info.path }/{filename}{.suffix}`,
+        expiration: new Date().getTime() + 600 /* 过期时间，在当前时间+10分钟 */
+    }
+    const policy =  btoa(JSON.stringify(policyObj))
+
+    const { HexMD5, b64hamcsha1 }  = (window || {}) as any
+
+    /* 计算 Authorization */
+    const passwordMd5 = HexMD5.MD5(info.password).toString(HexMD5.enc.Hex)
+
+    /* [Method-请求方法, URI-请求路径, policy] */
+    const arr = ['POST', `/${ info.bucket }`, policy]
+
+    const authorization = `UPYUN ${ info.name }:${ b64hamcsha1(passwordMd5, arr.join('&')) }`
+
+    return { policy, authorization }
+}
+
+/**
+ * @function base64ToFile
+ * @description base64转file对象
+ * @return { File } file对象
+ */
+export const base64ToFile = (base64String, name, type) => {
+    const byteCharacters = atob(base64String.split(',')[1])
+    const byteArrays: any[] = []
+
+    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+        const slice = byteCharacters.slice(offset, offset + 512)
+        const byteNumbers = new Array(slice.length)
+
+        for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i)
+        }
+
+        const byteArray = new Uint8Array(byteNumbers)
+        byteArrays.push(byteArray)
+    }
+
+    const blob = new Blob(byteArrays, { type })
+
+    return new File([blob], name, { type }) as File
+}
+
+/**
+ * @function getUrlBase64 图片转base64
+ * @param { String } url 图片链接
+ * @param { String } type 转图片的格式
+ * @return { String } base64
+ */
+export const fileUrlToBase64 = (url:string, type: string): object => {
+    return new Promise((resolve: any) => {
+        let canvas: any = document.createElement('canvas')
+        const ctx: any = canvas.getContext('2d')
+        const img = new Image()
+        img.crossOrigin = 'Anonymous'
+        img.onload = () => {
+            canvas.width = img.width
+            canvas.height = img.height
+            ctx.drawImage(img, 0, 0, img.width, img.height)
+            const dataURL = canvas.toDataURL('image/' + type)
+            canvas = null
+            const data = {
+                url: dataURL,
+                w: img.width,
+                h: img.height
+            }
+            resolve(data)
+        }
+        img.src = url
+    })
+}
