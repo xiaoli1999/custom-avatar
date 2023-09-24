6dfaf78c1f8f70e4fbabf9af10a30cdd5c3809ed
小黎（家） <22708206@qq.com>
2023-09-24 23:37:38 +0800

    feat(main.js): 导入message静态css资源
    导入message静态css资源

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/index.less'
+/* 单独引入message css */
+import 'element-plus/es/components/message/style/css'
 
 const app = createApp(App)
 app.mount('#app')
 
-console.log('%c 🐇年春节头像定制🌈 | 黎 | https://github.com/xiaoli1999/custom-rabbitImage ', 'color: #f4f4f4;background: #444; padding:5px 0;border-radius:2px;')
+console.log('%c 定制头像🌈 | 黎 | https://github.com/xiaoli1999/custom-avatar', 'color: #f4f4f4;background: #444; padding:5px 0;border-radius:2px;')
 
